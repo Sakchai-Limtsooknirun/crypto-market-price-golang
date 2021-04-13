e5e26f9cf28c323061c1d5593e3dcd50443b6251
sakchai jay <sakchai.l@ku.th>
2021-04-14 03:26:37 +0700

    initial

---
+++ model/market_currency_data.go
@@ -0,0 +1,36 @@
+package model
+
+import "time"
+
+type MarketCurrencyData []struct {
+	ID                           string      `json:"id"`
+	Symbol                       string      `json:"symbol"`
+	Name                         string      `json:"name"`
+	Image                        string      `json:"image"`
+	CurrentPrice                 float64     `json:"current_price"`
+	MarketCap                    float64     `json:"market_cap"`
+	MarketCapRank                float64     `json:"market_cap_rank"`
+	FullyDilutedValuation        interface{} `json:"fully_diluted_valuation"`
+	TotalVolume                  float64     `json:"total_volume"`
+	High24H                      float64     `json:"high_24h"`
+	Low24H                       float64     `json:"low_24h"`
+	PriceChange24H               float64     `json:"price_change_24h"`
+	PriceChangePercentage24H     float64     `json:"price_change_percentage_24h"`
+	MarketCapChange24H           float64     `json:"market_cap_change_24h"`
+	MarketCapChangePercentage24H float64     `json:"market_cap_change_percentage_24h"`
+	CirculatingSupply            float64     `json:"circulating_supply"`
+	TotalSupply                  interface{} `json:"total_supply"`
+	MaxSupply                    interface{} `json:"max_supply"`
+	Ath                          float64     `json:"ath"`
+	AthChangePercentage          float64     `json:"ath_change_percentage"`
+	AthDate                      time.Time   `json:"ath_date"`
+	Atl                          float64     `json:"atl"`
+	AtlChangePercentage          float64     `json:"atl_change_percentage"`
+	AtlDate                      time.Time   `json:"atl_date"`
+	Roi                          struct {
+		Times      float64 `json:"times"`
+		Currency   string  `json:"currency"`
+		Percentage float64 `json:"percentage"`
+	} `json:"roi"`
+	LastUpdated time.Time `json:"last_updated"`
+}
